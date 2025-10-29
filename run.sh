309bb33ed4318df133e61a2097f30f0d09a8663f
huysuy05 <huynguyen23@augustana.edu>
2025-10-29 15:44:42 -0500

    Added old marketplaces to placeholder + Ran scraper for a new markeplace

--- run.sh
+++ run.sh
@@ -3,5 +3,9 @@ pgrep -a tor || ps aux | grep -i tor | grep -v grep
 # Check listening ports (look for 9050 or 9150)
 lsof -nP -iTCP -sTCP:LISTEN | grep -E '9050|9150' || netstat -an | grep LISTEN | grep -E '9050|9150'
 
+# Start tor and privoxy in HomeBrew
+brew services start tor
+brew services start privoxy
+
 # Run scrape.py
 python scrape_simple.py --socks --socks-port 9050 --manual --disable-js
