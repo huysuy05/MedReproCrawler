e42b2068ca68185b044114c698fb3d522d1493e8
hieunguyen2711 <davidthehieu@gmail.com>
2025-11-01 13:38:11 -0500

    Added new todo

--- parser.py
+++ parser.py
@@ -18,6 +18,7 @@ TODO
   - Category (already added via category_page)
   - Any other fields from different marketplaces (Countries ship from, etc)
   - Vendor
+  - (Maybe) Format the time crawled/parsed into a better format for easier analysis.
 - Consider removing data processing (eg. Splitting, stripping, etc) to store raw data
 """
 
